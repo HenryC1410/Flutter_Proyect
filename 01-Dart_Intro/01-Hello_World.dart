da3a809d2c5e9913efc2f6825722418645eb3605
Henry Cordova <henrygerman37@gmail.com>
2025-05-23 21:03:30 -0500

    23/05

--- 01-Dart_Intro/01-Hello_World.dart
+++ 01-Dart_Intro/01-Hello_World.dart
@@ -10,12 +10,13 @@
 void main(){
 
   /* Hay varias formas de declarar un variable en dart las cuales son las siguientes:
-    var MyName = 'Henry';
-    String MyName ='Henry';
-    final MyName = 'Henry'; (final no te permite cambiar el valor de la variable) 
-    late final MyName; ("late" se utiliza para una variable final que aun no es asignada pero en algun momento lo hara)
+    1. var MyName = 'Mundo';
+    2. String MyName ='Mundo';
+    3. final MyName = 'Henry'; (final no te permite cambiar el valor de la variable) 
+      3.1 late final MyName; ("late" se utiliza para una variable final que aun no es asignada pero en algun momento lo hara)
+        3.1.1 MyName = 'Henry';
   */
-
+  
   const MyName = 'Henry';
 
   print('hola $MyName');
